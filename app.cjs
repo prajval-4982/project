8c9406b5b9172cc8aca67b23dbfe81326451a080
Prajwal Rathod <ptr4982@gmail.com>
2025-07-22 19:41:18 +0530

    Update backend: .env removed, .gitignore added, app changes

--- app.cjs
+++ app.cjs
@@ -26,10 +26,13 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // CORS
-app.use(cors({ origin: `http://localhost:5173`
-  //origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  // credentials: true
-}));
+app.use(
+  cors({
+    origin: ['https://vastram-frontend.onrender.com', 'http://localhost:5173'],
+    credentials: true,
+  })
+);
+
 
 // Body parser
 app.use(express.json({ limit: '10mb' }));
